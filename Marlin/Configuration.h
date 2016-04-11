f511d9c58137b6315dc915f728cb9a86f47702b5
AndrzejF <62.andrzej@gmail.com>
2016-04-11 19:52:28 +0200

    Zmiana rozmiaru osi Y

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -405,7 +405,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS 190
-#define Y_MAX_POS 170
+#define Y_MAX_POS 160
 #define Z_MAX_POS 120
 
 //===========================================================================
