a3c46b98b50f9ccf92c851013ee1527658908471
Andrzejf <62.andrzej@gmail.com>
2016-05-08 19:06:59 +0200

    Zmiana kroku silnika dla extrudera Titan

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -598,8 +598,8 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {82,82,4000,500}  // X i Y przeliczone na podstawie wydruku kontrolnego, Z i E pozostawione bez zmian
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {82,82,4000,714}  // X i Y przeliczone na podstawie wydruku kontrolnego, Z pozostawione bez zmian, E na podstawie rzeczywistego przesuwania filamentu
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.4,80.4,4000,714}  // X i Y przeliczone na podstawie wydruku kontrolnego, Z pozostawione bez zmian, E na podstawie rzeczywistego przesuwania filamentu
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.4,80.4,4000,456}  // X i Y przeliczone na podstawie wydruku kontrolnego, Z pozostawione bez zmian, E - dla Extrudera Titan E3D
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.4,80.4,4000,714}  // X i Y przeliczone na podstawie wydruku kontrolnego, Z pozostawione bez zmian, E na podstawie rzeczywistego przesuwania filamentu
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.4,80.4,4000,456}  // X i Y przeliczone na podstawie wydruku kontrolnego, Z pozostawione bez zmian, E - dla Extrudera Titan E3D
 
 #define DEFAULT_MAX_FEEDRATE          {300, 300, 5, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {3000,3000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
